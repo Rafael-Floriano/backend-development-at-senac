67841c54db3e9a33c08e0887af1d7eb75f3ab0c5
Rafael Floriano <rafael.floriano@gmail.com>
2024-05-02 20:58:13 -0300

    FEAT: added relation with Agenda object OneToOne

--- agenda/src/main/java/com/example/demo/model/Contato.java
+++ agenda/src/main/java/com/example/demo/model/Contato.java
@@ -1,6 +1,8 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,5 +18,7 @@ public class Contato implements Serializable {
     private Long id;
     private String nome;
     private String telefone;
+    @OneToOne(fetch = FetchType.EAGER)
+    private Agenda agenda;
 
 }
