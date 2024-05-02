9f57e4481e1015e3f49a8518619874236a5d7323
Rafael Floriano <rafael.floriano@gmail.com>
2024-05-02 20:59:27 -0300

    FEAT: added association in Agenda with object 'usuario' (OneToOne) and Contato(OneToMany)

--- agenda/src/main/java/com/example/demo/model/Agenda.java
+++ agenda/src/main/java/com/example/demo/model/Agenda.java
@@ -1,11 +1,15 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @Entity
@@ -15,5 +19,11 @@ public class Agenda implements Serializable {
 
     private Long id;
     private String titulo;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Usuario usuario;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Contato> contato;
 
 }
