1191d3b36712698f990eed7ebc9500fed76ca7ee
Rafael Floriano <rafael.floriano@gmail.com>
2024-05-02 20:26:30 -0300

    FEAT: implemented new method to delete users by id and added 'validated @NotNull'

--- ads3/src/main/java/com/br/ads3/services/UserService.java
+++ ads3/src/main/java/com/br/ads3/services/UserService.java
@@ -3,16 +3,19 @@ package com.br.ads3.services;
 import com.br.ads3.entities.Usuario;
 import com.br.ads3.repository.UsuarioRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 
 @Slf4j
 @Service
+@Validated
 @RequiredArgsConstructor
 public class UserService {
 
@@ -27,6 +30,11 @@ public class UserService {
     @Transactional(rollbackOn = Exception.class)
     public List<Usuario> showAllUsers() {
         return usuarioRepository.findAll();
+    }
+
+    @Transactional(rollbackOn = Exception.class)
+    public void deleteUseById(@NotNull Integer userId) {
+        usuarioRepository.deleteById(userId);
     }
 
 }
