1ffa1aad19e1fef27d9d2c3289863046a5272ebd
Rafael Floriano <rafael.floriano@gmail.com>
2024-05-02 20:48:24 -0300

    FEAT: added new model 'Agenda' in agenda project

---
+++ agenda/src/main/java/com/example/demo/model/Agenda.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Agenda implements Serializable {
+
+    private Long id;
+    private String titulo;
+
+}
