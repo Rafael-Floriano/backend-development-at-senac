ec01b2e381d74d4c587a8345df402d6cf9f54770
Rafael Floriano <rafael.floriano@gmail.com>
2024-05-02 19:45:04 -0300

    feat: added new transactional method to save new users called as 'createUser'

--- ads3/src/main/java/com/br/ads3/services/UserService.java
+++ ads3/src/main/java/com/br/ads3/services/UserService.java
@@ -1,4 +1,24 @@
 package com.br.ads3.services;
 
+import com.br.ads3.entities.Usuario;
+import com.br.ads3.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional(rollbackOn = Exception.class)
+    public void createUser(Usuario usuario) {
+        usuarioRepository.save(usuario);
+    }
+
 }
