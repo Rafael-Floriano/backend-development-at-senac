0525f760e8d2a44463c39a0f3de7ea238bbed7eb
Rafael Floriano <rafael.floriano@gmail.com>
2024-05-02 19:47:18 -0300

    refactor: removed service logic in controller and added called of methods in services

--- ads3/src/main/java/com/br/ads3/controllers/UsuarioController.java
+++ ads3/src/main/java/com/br/ads3/controllers/UsuarioController.java
@@ -1,16 +1,22 @@
 package com.br.ads3.controllers;
 
+import com.br.ads3.entities.Usuario;
+import com.br.ads3.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.br.ads3.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
 
     @Autowired
-    UsuarioRepository repository;
+    UserService userService;
 
+    @ResponseStatus(value = HttpStatus.CREATED, reason = "User has been created!")
+    public void createUser(Usuario usuario) {
+        userService.createUser(usuario);
+    }
 }
