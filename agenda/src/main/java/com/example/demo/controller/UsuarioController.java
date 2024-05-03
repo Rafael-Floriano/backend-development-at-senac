697e765fdedef6d9171a8c77fe7523fb133259ac
Rafael Floriano <rafael.floriano@gmail.com>
2024-05-02 21:19:13 -0300

    FEAT: implemented method 'createNewUser' in controller at project agenda

--- agenda/src/main/java/com/example/demo/controller/UsuarioController.java
+++ agenda/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,4 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Usuario;
+import com.example.demo.service.UsuarioService;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/criar")
+    @ResponseStatus(value = HttpStatus.CREATED, reason = "Usuario foi criado com sucesso!")
+    public void createNewUser(@RequestBody @NotNull Usuario usuario) {
+        usuarioService.createNewUser(usuario);
+    }
+
 }
