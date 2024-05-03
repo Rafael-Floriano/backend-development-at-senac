da7a12bca4f8f41d0393363a8bcc1c069c81c246
Rafael Floriano <rafael.floriano@gmail.com>
2024-05-02 21:18:27 -0300

    FEAT: added new Transactional  method 'createnewUser' in userService

---
+++ agenda/src/main/java/com/example/demo/service/UsuarioService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Usuario;
+import com.example.demo.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Slf4j
+@Service
+@Validated
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional(rollbackOn = Exception.class)
+    public void createNewUser(@NotNull Usuario usuario) {
+        usuarioRepository.save(usuario);
+    }
+
+}
