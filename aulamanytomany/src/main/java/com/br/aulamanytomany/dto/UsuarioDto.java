9f585c13cdc5ce2badfd2fb922903accbced875f
rafael <rafael.florianoc9@gmail.com>
2024-05-24 19:55:47 -0300

    FEAT: implemented method to convert Usuario entity to UsuarioDto

--- aulamanytomany/src/main/java/com/br/aulamanytomany/dto/UsuarioDto.java
+++ aulamanytomany/src/main/java/com/br/aulamanytomany/dto/UsuarioDto.java
@@ -1,4 +1,25 @@
 package com.br.aulamanytomany.dto;
 
-public class UsuarioDto {
+import com.br.aulamanytomany.entities.Contato;
+import com.br.aulamanytomany.entities.Usuario;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class UsuarioDto implements Serializable {
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    List<ContatoDto> contatoList;
+
+    public UsuarioDto(Usuario usuario) {
+        this.id = usuario.getId();
+        this.nome = usuario.getNome();
+        this.cpf = usuario.getCpf();
+        this.email = usuario.getEmail();
+        this.contatoList = usuario.getContatoList().stream().map(ContatoDto::new).toList();
+    }
+
 }
