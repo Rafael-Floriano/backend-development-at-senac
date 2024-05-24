a73903e8beeeb58e26653e69df00131b84158278
rafael <rafael.florianoc9@gmail.com>
2024-05-24 19:55:34 -0300

    FEAT: implemented method to convert contato entity to contatoDto

--- aulamanytomany/src/main/java/com/br/aulamanytomany/dto/ContatoDto.java
+++ aulamanytomany/src/main/java/com/br/aulamanytomany/dto/ContatoDto.java
@@ -1,4 +1,23 @@
 package com.br.aulamanytomany.dto;
 
-public class ContatoDto {
+import com.br.aulamanytomany.entities.Contato;
+import com.br.aulamanytomany.entities.Usuario;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+
+public class ContatoDto implements Serializable {
+
+    private int id;
+    private String nome;
+    private String telefone;
+    private int idUsuario;
+
+    public ContatoDto(Contato contato) {
+        this.id = contato.getId();
+        this.nome = contato.getNome();
+        this.telefone = contato.getTelefone();
+        this.idUsuario = contato.getUsuario().getId();
+    }
+
 }
