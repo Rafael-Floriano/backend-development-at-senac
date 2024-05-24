a4c53c935b0675e200facc74c518de0f1a5c3538
rafael <rafael.florianoc9@gmail.com>
2024-05-24 20:02:12 -0300

    FEAT: implemented new method buscaUsuarios in controller 'UsuarioController'

--- aulamanytomany/src/main/java/com/br/aulamanytomany/controllers/UsuarioController.java
+++ aulamanytomany/src/main/java/com/br/aulamanytomany/controllers/UsuarioController.java
@@ -1,10 +1,24 @@
 package com.br.aulamanytomany.controllers;
 
+import com.br.aulamanytomany.dto.UsuarioDto;
+import com.br.aulamanytomany.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping("/todos")
+    public List<UsuarioDto> listarUsuarios() {
+        return usuarioService.buscaUsuarios();
+    }
 
 }
